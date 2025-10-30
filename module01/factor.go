85b77fd9f8bb665ab26e127b4c340d924a291cf9
karalarmehmet <mehmetkaralarm@gmail.com>
2025-10-30 23:28:56 +0300

    solve factor

--- module01/factor.go
+++ module01/factor.go
@@ -11,16 +11,26 @@ package module01
 //
 // Examples:
 //
-//   Factor([]int{2,3,5}, 30) // []int{2,3,5}
-//   Factor([]int{2,3,5}, 28) // []int{2,2,7}
-//   Factor([]int{2,3,5}, 720) // []int{2,2,2,2,3,3,5}
+//	Factor([]int{2,3,5}, 30) // []int{2,3,5}
+//	Factor([]int{2,3,5}, 28) // []int{2,2,7}
+//	Factor([]int{2,3,5}, 720) // []int{2,2,2,2,3,3,5}
 //
 // Examples with remainders:
 //
-//   Factor([2,5], 30) // []int{2,5,3}
-//   Factor([3,5], 720) // []int{3,3,5,16}
-//   Factor([], 4) // []int{4}
-//
+//	Factor([2,5], 30) // []int{2,5,3}
+//	Factor([3,5], 720) // []int{3,3,5,16}
+//	Factor([], 4) // []int{4}
 func Factor(primes []int, number int) []int {
-	return nil
+	result := []int{}
+	for _, v := range primes {
+		for number%v == 0 {
+			result = append(result, v)
+			number = number / v
+		}
+	}
+	if number > 1 {
+		result = append(result, number)
+	}
+
+	return result
 }
